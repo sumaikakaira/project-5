b63413e0e4eb95553c44ce50d46d06d6cfb4a887
sumaikakaira <122632122+sumaikakaira@users.noreply.github.com>
2023-04-13 12:18:50 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,53 @@
+// assignment 5
+           //question no 1
+//var city=prompt("you live in city");
+ //if(city==="karachi") {
+   // alert('welcome to city of lights');
+// }  
+//else{
+//alert('sorry!acceptable')
+ //}  ;
+ //question no 2
+ //var person=prompt("you are gender");
+ //if(gender==="female"){
+  //  alert("good morning maam!");
+ //}
+ //else{
+   // alert('not acceptable')
+// };
+ //if(gender==="male"){
+    //alert("good morning sir!");
+ //}
+// else{
+   // alert("not acceptable")
+ //};
+// if(gender==="notfemale or male"){
+   // alert('you are robot')
+//};
+//question 3
+//var color=prompt("color of road traffic signal");
+//if(color==="red"){
+    //alert("must stop")
+//}
+//else{
+//    alert('not stop')
+//};
+//if(color==='yellow'){
+    //alert("ready to move")
+//}
+//else{
+   // alert('not move')
+//};
+//if(color==='green'){
+    //alert('move now')
+//}
+//else{
+  //  alert("not movable")
+    
+    
+//};
+//questionb 4
+var car=prompt("fuel in your car");
+if(current==="less than 0.25litres"){
+    alert('please refill the fuel in your cat')
+};
